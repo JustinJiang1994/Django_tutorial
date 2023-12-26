9204e7f4404bc65e6c3f737ccb341b4bf9ae46cc
Justin Jiang <js830910@gmail.com>
2023-12-26 16:56:12 +0800

    完成login界面搭建

--- 2_login/login/demo/views.py
+++ 2_login/login/demo/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def login(request):
+    # 判断到底是POST还是GET
+    if request.method == "GET":
+        # GET请求返回
+        return render(request, 'login.html')
+    else:
+        # POST：用户提交的数据以post方式返回
+        # 去请求体中获取数据再进行校验
+        username = request.POST.get('user')
+        passwoard = request.POST.get('pwd')
+
+        # 获得后就可以去数据库中校验用户名+密码的合法性
+        # 校验成功后跳转至后台管理界面 http://127.0.0.1:8000/index/ /index/
+        if username == "root" and passwoard == "123":
+            return redirect('/index/')
+        
+        # 不成功，再次让用户看到login.html页面，并提示用户名或密码错误
+        return render(request, 'login.html', {"error": "用户名或密码错误"}) 
+    
+
+def index(request):
+    return render(request, 'index.html')
--- 2_login/login/login/urls.py
+++ 2_login/login/login/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from demo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login),
+    path('index/', views.index),
 ]
